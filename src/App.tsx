722a0be1f54f5a88c8c55531345847af50c5eddd
MASSHUU12 <gawrysiak.maciej@protonmail.com>
2024-09-13 15:11:06 +0200

    feat: allow generating & deleting words

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,89 @@
-import { Button } from "@mui/material";
-import React from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+import { generateWords } from "./lib/WordGenerator";
 
 function App(): React.JSX.Element {
+  const [words, setWords] = useState<string[]>([]);
+  const [checked, setChecked] = useState<number[]>([]);
+
+  const handleToggle = (index: number): void => {
+    const currentIndex = checked.indexOf(index);
+    const newChecked = [...checked];
+
+    if (currentIndex === -1) {
+      newChecked.push(index);
+    } else {
+      newChecked.splice(currentIndex, 1);
+    }
+
+    setChecked(newChecked);
+  };
+
+  const handleGenerate = (): void => {
+    setWords(generateWords(10, 1, 6));
+    setChecked([]);
+  };
+
+  const handleClear = (): void => {
+    setWords([]);
+    setChecked([]);
+  };
+
   return (
-    <main className="flex justify-center ">
-      <Button variant="contained">Hello world</Button>
-    </main>
+    <Box
+      component="main"
+      minHeight={"100svh"}
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+    >
+      <Typography variant="h1">Word Generator</Typography>
+      <Stack direction="row" spacing={2} justifyContent="center">
+        <Button variant="contained" onClick={handleGenerate}>
+          Generate
+        </Button>
+        <Button variant="contained" onClick={undefined}>
+          Mutate Selected
+        </Button>
+        <Button variant="contained" onClick={handleClear}>
+          Clear
+        </Button>
+      </Stack>
+      <List>
+        {words.map((word: string, i: number) => (
+          <ListItem key={word} disablePadding>
+            <ListItemButton
+              role="button"
+              onClick={() => handleToggle(i)}
+              dense
+              disableRipple
+            >
+              <ListItemIcon>
+                <Checkbox
+                  edge="start"
+                  checked={checked.includes(i)}
+                  tabIndex={-1}
+                  disableRipple
+                  inputProps={{ "aria-labelledby": word }}
+                />
+              </ListItemIcon>
+              <ListItemText id={word} primary={word} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
   );
 }
 
